af22a607addba7de4bca56869a251429f487114b
Xie Yi <wez733@live.cn>
2018-11-02 14:02:50 +0800

    allow get dataset without login

--- src/main/java/org/ai/carp/controller/dataset/DatasetAllController.java
+++ src/main/java/org/ai/carp/controller/dataset/DatasetAllController.java
@@ -1,9 +1,7 @@
 package org.ai.carp.controller.dataset;
 
-import org.ai.carp.controller.util.UserUtils;
 import org.ai.carp.model.Database;
 import org.ai.carp.model.dataset.Dataset;
-import org.ai.carp.model.user.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +15,6 @@ public class DatasetAllController {
 
     @GetMapping
     public DatasetAllResponse get(HttpSession session) {
-        UserUtils.getUser(session, User.USER);
         List<Dataset> datasets = Database.getInstance().getDatasets().findAll();
         return new DatasetAllResponse(datasets);
     }
