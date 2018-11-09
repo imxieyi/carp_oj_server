598af8c3af183ab5dcdec15e6f677e94e9bd72c5
Xie Yi <wez733@live.cn>
2018-11-09 19:16:05 +0800

    also return 401 when logout without login

--- src/main/java/org/ai/carp/controller/login/LogoutController.java
+++ src/main/java/org/ai/carp/controller/login/LogoutController.java
@@ -1,6 +1,6 @@
 package org.ai.carp.controller.login;
 
-import org.ai.carp.controller.exceptions.InvalidRequestException;
+import org.ai.carp.controller.exceptions.NotLoggedInException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +15,7 @@ public class LogoutController {
     public LogoutResponse get(HttpSession session) {
         String uid = (String) session.getAttribute("uid");
         if (uid == null) {
-            throw new InvalidRequestException("Not logged in!");
+            throw new NotLoggedInException("Not logged in!");
         }
         session.invalidate();
         return new LogoutResponse(uid);
