2dcfd79e7442394e5f3d3e0d4a2427fe8caa4c17
Xie Yi <wez733@live.cn>
2018-11-26 13:29:25 +0800

    get output admin html view

--- src/main/java/org/ai/carp/controller/admin/judge/GetOutputController.java
+++ src/main/java/org/ai/carp/controller/admin/judge/GetOutputController.java
@@ -5,21 +5,20 @@ import org.ai.carp.controller.util.UserUtils;
 import org.ai.carp.model.Database;
 import org.ai.carp.model.judge.CARPCase;
 import org.ai.carp.model.user.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 import java.util.Optional;
 
-@RestController
-@RequestMapping("/api/admin/judge/output")
+@Controller
 public class GetOutputController {
 
-    @GetMapping
-    public OutputResponse get(@RequestParam("cid") String cid, HttpSession session) {
+    @GetMapping("/admin/judge/details")
+    public String get(@RequestParam("cid") String cid, Model model, HttpSession session) {
         UserUtils.getUser(session, User.ADMIN);
         if (StringUtils.isEmpty(cid)) {
             throw new InvalidRequestException("No cid!");
@@ -32,32 +31,10 @@ public class GetOutputController {
         if (carpCase.getStatus() != CARPCase.FINISHED && carpCase.getStatus() != CARPCase.ERROR) {
             throw new InvalidRequestException("Case has not finished!");
         }
-        return new OutputResponse(carpCase.getExitcode(), carpCase.getStdout(), carpCase.getStderr());
+        model.addAttribute("exitcode", carpCase.getExitcode());
+        model.addAttribute("stdout", carpCase.getStdout());
+        model.addAttribute("stderr", carpCase.getStderr());
+        return "output";
     }
 
-}
-
-class OutputResponse {
-
-    private int exitcode;
-    private String stdout;
-    private String stderr;
-
-    public OutputResponse(int exitcode, String stdout, String stderr) {
-        this.exitcode = exitcode;
-        this.stdout = stdout;
-        this.stderr = stderr;
-    }
-
-    public int getExitcode() {
-        return exitcode;
-    }
-
-    public String getStdout() {
-        return stdout;
-    }
-
-    public String getStderr() {
-        return stderr;
-    }
 }
