ac314a3af3cdbf006377b0094f3e4dbad7bf55cd
edwardfang <fangyd1997@outlook.com>
2018-11-29 19:49:48 +0800

    add ratio bias check

--- src/main/java/org/ai/carp/controller/util/ISEUtils.java
+++ src/main/java/org/ai/carp/controller/util/ISEUtils.java
@@ -12,8 +12,20 @@ public class ISEUtils {
         double stdInfluence = iseCase.getDataset().getInfluence();
         String stdout = iseCase.getStdout();
         iseCase.setInfluence(Double.valueOf(stdout));
-        iseCase.setValid(true);
-        iseCase.setReason("Solution accepted");
+        boolean result = checkIsLargeBias(stdInfluence, iseCase.getInfluence(), 0.001);
+        iseCase.setValid(!result);
+        if (result) {
+            iseCase.setReason("Bias Too Large");
+        } else {
+            iseCase.setReason("Solution accepted");
+        }
+
+    }
+
+    private static boolean checkIsLargeBias(double standard, double student, double toleranceRatio) {
+        double max_bias = standard * toleranceRatio;
+        double bias = Math.abs(standard - student);
+        return bias > max_bias;
     }
     
 }
